d9a844db62bc6d2cc10c7ea1ffaa1d0450e4d446
Johnathon Selstad <makeshifted@gmail.com>
2025-07-16 19:18:55 -0700

    Think in terms of chunks

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoade
 const { MeshoptDecoder } = await import( '../node_modules/three/examples/jsm/libs/meshopt_decoder.module.js' );
 import { PlayerController } from './PlayerController.js';
 import { ADDITION, INTERSECTION, SUBTRACTION, Brush, Evaluator } from '../node_modules/three-bvh-csg/build/index.module.js';
+import { MeshBVH, MeshBVHHelper, StaticGeometryGenerator } from '../node_modules/three-mesh-bvh/build/index.module.js';
 
 /** The fundamental set up and animation structures for Simulation */
 export default class Main {
@@ -31,6 +32,7 @@ export default class Main {
 
             displayCollider: false,
             displayBVH: false,
+            displayGround: true,
             visualizeDepth: 10,
             gravity: - 30,
             playerSpeed: 10,
@@ -54,6 +56,7 @@ export default class Main {
         let visFolder = this.gui.addFolder( 'Visualization' );
         visFolder.add( this.simulationParams, 'displayCollider' ).onChange( v => { this.collider.visible = v; } );
         visFolder.add( this.simulationParams, 'displayBVH' ).onChange( v => { this.visualizer.visible = v; } );
+        visFolder.add( this.simulationParams, 'displayGround' ).onChange( v => { this.mesh.visible = v; } );
         visFolder.add( this.simulationParams, 'visualizeDepth', 1, 20, 1 ).onChange( v => {
             this.visualizer.depth = v;
             this.visualizer.update();
@@ -78,11 +81,25 @@ export default class Main {
         this.evaluator = new Evaluator();
 
         // Create a plane to render the raytraced shader material
-        this.planeGeometry = new THREE.BoxGeometry( 100.0, 100.0, 100.0 );
+        this.chunkGeometry = new THREE.BoxGeometry( 10.0, 10.0, 10.0 );
+        let bbox = new THREE.Box3( new THREE.Vector3( -5.0, -5.0, -5.0 ), new THREE.Vector3( 5.0, 5.0, 5.0 ) );
         this.defaultMaterial = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0.5, metalness: 0.5 } );
-        this.mesh = new Brush( this.planeGeometry, this.defaultMaterial );
+        //this.mesh = new Brush( this.chunkGeometry, this.defaultMaterial );
+        this.chunks = [];
+        this.mesh = new THREE.Group();
+        for( let x = 0; x < 10; x++ ) {
+            for( let z = 0; z < 10; z++ ) {
+                let chunk = new Brush( this.chunkGeometry, this.defaultMaterial );
+                chunk.position.set( x * 10.0 - 45.0, - 5.0, z * 10.0 - 45.0 );
+                //chunk.boundsTree = this.chunkBVH;
+                chunk.updateMatrixWorld( true );
+                //chunk.geometry.computeBoundingBox();
+                chunk.bbox = bbox.clone().applyMatrix4( chunk.matrixWorld );
+                this.mesh.add( chunk );
+                this.chunks.push( chunk );
+            }
+        }
         this.mesh.receiveShadow = true;
-        this.mesh.position.set( 0, - 50.0, 0 );
         this.mesh.updateMatrixWorld( true );
 
         // Create the player controller
@@ -100,16 +117,15 @@ export default class Main {
         //this.loadColliderEnvironment();
         this.updateEnvironment( this.mesh );
 
+        this.ePressed = false;
         window.addEventListener('keydown', (e) => {
             switch (e.code) {
                 case 'KeyE':
-                    this.world.scene.remove(this.mesh);
-                    this.mesh = this.evaluator.evaluate( this.mesh, this.brush2, ADDITION );
-                    this.updateEnvironment( this.mesh );
-                    this.world.scene.add( this.mesh );
+                    this.ePressed = true;
                     break;
             }
         });
+        this.frameNum = 0;
     }
 
     loadColliderEnvironment() {
@@ -152,14 +168,35 @@ export default class Main {
         this.world.controls.update();
 
         // Cast a ray against the environment collider and place the brush2 there
-        this.hitPoint = this.player.raycastAgainstCollider(this.player.position, new THREE.Vector3().copy(this.player.position).sub(this.world.camera.position).normalize());
-        if (this.hitPoint) {
-            this.brush2.position.copy(this.hitPoint);
-            this.brush2.updateMatrixWorld();
+        this.brush2.position.copy(new THREE.Vector3().copy(this.player.position).sub(this.world.camera.position).normalize().multiplyScalar(3).add(this.player.position));
+        this.brush2.updateMatrixWorld();
+
+        if ( this.ePressed || this.frameNum === 0) { //true){//
+            this.ePressed = false;
+            let dirty = false;
+            //this.world.scene.remove(this.mesh);
+            //this.mesh = this.evaluator.evaluate( this.chunks, this.brush2, SUBTRACTION);
+
+            let box1 = new THREE.Box3();
+            box1.setFromObject(this.brush2);
+
+            for(let i = 0; i < this.chunks.length; i++) {
+                if (box1.intersectsBox(this.chunks[i].bbox) || this.frameNum === 0) {
+                    this.environment.remove(this.chunks[i]);
+                    this.chunks[i] = this.evaluator.evaluate( this.chunks[i], this.brush2, SUBTRACTION);
+                    this.chunks[i].updateMatrixWorld();
+                    this.chunks[i].geometry.computeBoundingBox();
+                    this.chunks[i].bbox = this.chunks[i].geometry.boundingBox.clone().applyMatrix4( this.chunks[i].matrixWorld );
+                    this.environment.add(this.chunks[i]);
+                    dirty = true;
+                }
+            }
+            if(dirty){this.updateEnvironment( this.mesh );}
         }
 
         this.world.renderer.render(this.world.scene, this.world.camera);
         this.world.stats.update();
+        this.frameNum++;
     }
 
     // Log Errors as <div>s over the main viewport
